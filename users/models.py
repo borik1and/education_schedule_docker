a307553dca9313fe591b5068f0950d011fdf7752
Boris <borik1and@gmail.com>
2024-01-25 20:19:50 +0200

    add app users and config

---
+++ users/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='Почта')
+
+    phone = models.CharField(max_length=35, verbose_name='Телефон', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
+    country = models.CharField(max_length=50, verbose_name='Страна', **NULLABLE)
+    is_active = models.BooleanField(default=False, verbose_name='Активный')
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
