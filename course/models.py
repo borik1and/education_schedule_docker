4b300910aa7ee1fc4ea9be0ca331f140c7727294
Boris <borik1and@gmail.com>
2024-01-26 09:57:19 +0200

    add app lesson and course

--- course/models.py
+++ course/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(max_length=150, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+    preview = models.IntegerField(verbose_name='превью')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
