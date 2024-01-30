821ee32b2808d78df15132366c4b8dbe23358b3e
Boris <borik1and@gmail.com>
2024-01-30 16:27:14 +0200

    add command fill

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
 from rest_framework.routers import DefaultRouter
 
-from users.views import UserViewSet
+from users.views import UserViewSet, PaymentViewSet
 
 app_name = 'user'
 
 router = DefaultRouter()
 router.register(r'user', UserViewSet, basename='user')
+router.register(r'pay', PaymentViewSet, basename='pay')
 
 urlpatterns = [
 
