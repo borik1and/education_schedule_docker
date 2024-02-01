1e12594c8d48f229e66e2b5a5061638d0e482916
Boris <borik1and@gmail.com>
2024-02-01 14:57:58 +0200

    add lessons line in course

--- course/serializers.py
+++ course/serializers.py
@@ -3,13 +3,6 @@ from course.models import Course
 from lesson.serializers import LessonSerializer
 
 
-# Для сериализатора для модели курса реализуйте поле вывода уроков.
-# Вывод реализуйте с помощью сериализатора для связанной модели.
-#
-# Один сериализатор должен выдавать и количество уроков
-# курса и информацию по всем урокам курса одновременно.
-
-
 class CourseSerializer(serializers.ModelSerializer):
     lessons_count = serializers.SerializerMethodField()
     lessons = LessonSerializer(many=True, read_only=True)
