6e9ce9c6969b5ac3e36b455d44c607fd971e068b
Boris <borik1and@gmail.com>
2024-02-15 20:09:39 +0200

    add def get_is_subscribed

--- course/urls.py
+++ course/urls.py
@@ -1,6 +1,7 @@
 from rest_framework.routers import DefaultRouter
-
+from django.urls import path
 from course.views import CourseViewSet
+from lesson.views import CourseSubscriptionView
 
 app_name = 'course'
 
@@ -8,4 +9,6 @@ router = DefaultRouter()
 router.register(r'course', CourseViewSet, basename='course')
 
 urlpatterns = [
+    path('<int:course_id>/course-subscribe/', CourseSubscriptionView.as_view(), name='course-subscribe'),
+    path('<int:course_id>/course-unsubscribe/', CourseSubscriptionView.as_view(), name='course-unsubscribe'),
 ] + router.urls
--- lesson/views.py
+++ lesson/views.py
@@ -1,10 +1,15 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
+from course.models import Course
 from lesson.models import Lesson
 from lesson.paginators import LessonPaginator
 from lesson.permissions import IsModerator, IsOwner
 from lesson.serializers import LessonSerializer
+from users.models import CourseSubscription
 
 
 class LessonCreateAPIView(generics.CreateAPIView):
@@ -39,3 +44,22 @@ class LessonRetrieveAPIView(generics.RetrieveAPIView):
 class LessonDestroyAPIView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
     permission_classes = [IsAuthenticated, ~IsModerator | IsOwner]
+
+
+class CourseSubscriptionView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, course_id):
+        course = get_object_or_404(Course, pk=course_id)
+        subscription, created = CourseSubscription.objects.get_or_create(user=request.user, course=course)
+        if created:
+            subscription.subscribed = True
+            subscription.save()
+            return Response({'detail': 'Subscription to course set successfully.'}, status=status.HTTP_201_CREATED)
+        return Response({'detail': 'You are already subscribed to this course.'}, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, course_id):
+        course = get_object_or_404(Course, pk=course_id)
+        subscription = get_object_or_404(CourseSubscription, user=request.user, course=course)
+        subscription.delete()
+        return Response({'detail': 'Subscription to course removed successfully.'}, status=status.HTTP_204_NO_CONTENT)
