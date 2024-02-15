6ef7eb61c2b283670db854ce30fc2ec51959e7af
Boris <borik1and@gmail.com>
2024-02-15 19:41:28 +0200

    add def get_is_subscribed

--- course/serializers.py
+++ course/serializers.py
@@ -6,9 +6,17 @@ from lesson.serializers import LessonSerializer
 class CourseSerializer(serializers.ModelSerializer):
     lessons_count = serializers.SerializerMethodField()
     lessons = LessonSerializer(many=True, read_only=True)
+    is_subscribed = serializers.SerializerMethodField()
 
     def get_lessons_count(self, instance):
         return instance.lessons.count()
+
+    def get_is_subscribed(self, instance):
+        request = self.context.get('request')
+        user = request.user if request else None
+        if user and user.is_authenticated:
+            return instance.subscribers.filter(user=user).exists()
+        return False
 
     class Meta:
         model = Course
