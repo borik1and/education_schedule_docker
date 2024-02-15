8f1b30f9e96e66af0ee13dfac454d8325baa87f9
Boris <borik1and@gmail.com>
2024-02-15 18:52:53 +0200

    changing code in def test_lesson_list

--- lesson/tests.py
+++ lesson/tests.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework_simplejwt.tokens import AccessToken
 from django.contrib.auth.models import User
 from django.test import TestCase
@@ -15,7 +16,7 @@ class LessonTestCase(TestCase):
         self.user = User.objects.create_user(username='testuser', password='password', is_active=True)
         self.token = AccessToken.for_user(self.user)  # Создаем токен для пользователя
 
-    def test_lesson_lesson(self):
+    def test_lesson_create(self):
         self.client.credentials(
             HTTP_AUTHORIZATION='Bearer ' + str(self.token))  # Устанавливаем токен в заголовке запроса
         url = reverse('lesson:create')
@@ -25,13 +26,28 @@ class LessonTestCase(TestCase):
             'video_url': 'youtube.com',
             'course': self.course.id
         }
-        response = self.client.post(url, data=data)
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(
-            response.json(),
-            {'title': 'Test Lesson', 'description': 'Test Description', 'video_url': 'youtube.com'}
+        response = self.client.post(url, data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['title'], 'Test Lesson')
+        self.assertEqual(response.data['description'], 'Test Description')
+        self.assertEqual(response.data['video_url'], 'youtube.com')
+        self.assertTrue(Lesson.objects.filter(title='Test Lesson').exists())
 
-        )
-        self.assertTrue(
-            Lesson.objects.all().exists
-        )
+    def test_lesson_list(self):
+        """ Test that we can list all lessons """
+        Lesson.objects.create(title='Test Lesson', description='Test Description', video_url='youtube.com',
+                              course=self.course)
+        self.client.credentials(
+            HTTP_AUTHORIZATION='Bearer ' + str(self.token))  # Устанавливаем токен в заголовке запроса
+        url = reverse('lesson:list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Проверяем, что в ответе есть данные
+        self.assertTrue(response.data)
+
+        # Проверяем, что в ответе есть ожидаемые данные урока
+        lesson_data = response.data['results'][0]  # Получаем первый урок из списка
+        self.assertEqual(lesson_data['title'], 'Test Lesson')
+        self.assertEqual(lesson_data['description'], 'Test Description')
+        self.assertEqual(lesson_data['video_url'], 'youtube.com')
