c2d0ad7c25d88b5a31604225462c60f24817834d
Boris <borik1and@gmail.com>
2024-02-21 19:51:44 +0200

    changing def process_payment

--- users/services.py
+++ users/services.py
@@ -1,33 +1,67 @@
 import stripe
 from django.conf import settings
-from django.http import JsonResponse
 from .models import Payment
-from .serializers import PaymentSerializer
+from django.http import JsonResponse
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
+def create_product(name):
+    product = stripe.Product.create(
+        name=name,
+        active=True
+    )
+    return product.id
+
+
+def create_price(product_id, amount):
+    price = stripe.Price.create(
+        product=product_id,
+        unit_amount=amount * 100,  # цены в Stripe указываются в копейках
+        currency='usd',
+    )
+    return price.id
+
+
+def create_checkout_session(price_id, success_url, cancel_url):
+    session = stripe.checkout.Session.create(
+        payment_method_types=['card'],
+        line_items=[{
+            'price': price_id,
+            'quantity': 1,
+        }],
+        mode='payment',
+        success_url=success_url,
+        cancel_url=cancel_url,
+    )
+    return session.id
+
+
 def process_payment(request):
     if request.method == 'POST':
-        token = request.POST.get('token')  # Токен, сгенерированный Stripe.js
-        amount = request.POST.get('amount')  # Сумма к оплате, в центах
+        # Получаем данные из запроса
+        amount = int(request.POST.get('amount'))
+        success_url = request.POST.get('success_url')
+        cancel_url = request.POST.get('cancel_url')
+        product_name = request.POST.get('product_name')
 
         try:
-            charge = stripe.Charge.create(
-                amount=amount,
-                currency='usd',
-                source=token,
-                description='Оплата за курс'
-            )
-            # Платеж успешно обработан
-            payment = Payment.objects.create(
-                user=request.user,  # Предполагается, что информация о пользователе доступна через аутентификацию
-                amount_pay=amount,
-                method_pay='card',  # Предполагается, что оплата осуществляется картой
+            # Создаем продукт и цену в Stripe
+            product_id = create_product(product_name)
+            price_id = create_price(product_id, amount)
 
+            # Создаем сессию для платежа в Stripe
+            session_id = create_checkout_session(price_id, success_url, cancel_url)
+
+            # Сохраняем ссылку на оплату в нашей системе
+            payment = Payment.objects.create(
+                amount_pay=amount,
+                product_name=product_name,
+                payment_session_id=session_id
             )
-            payment_serializer = PaymentSerializer(payment)
-            return JsonResponse({'message': 'Платеж успешно обработан', 'payment': payment_serializer.data})
-        except stripe.error.CardError as e:
-            # Ошибка при обработке платежа
+
+            # Возвращаем ответ с данными о платеже
+            return JsonResponse({'payment_session_id': session_id, 'payment_id': payment.id})
+        except stripe.error.StripeError as e:
+            # Обработка ошибок Stripe
             return JsonResponse({'error': str(e)}, status=402)
