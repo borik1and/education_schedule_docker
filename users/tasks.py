c3f86f0eda27b8d5a4c12e4bc6d934502b17a2d9
Boris <borik1and@gmail.com>
2024-02-28 19:50:20 +0200

    changing code in def if_user_not_logged_in

--- users/tasks.py
+++ users/tasks.py
@@ -2,12 +2,13 @@ from datetime import datetime, timedelta
 from users.models import User
 
 
-def if_user_not_logged_in(user_id):
-    user_login = User.objects.filter(id=user_id).first()
-    if user_login and user_login.last_login:
-        today = datetime.now().date()
-        last_login_date = user_login.last_login.date()
-        thirty_days_ago = today - timedelta(days=30)
-        if last_login_date <= thirty_days_ago:
-            user_login.is_active = False
-            user_login.save()
+def if_user_not_logged_in():
+    users = User.objects.all()
+    today = datetime.now().date()
+    for user_login in users:
+        if user_login.last_login:
+            last_login_date = user_login.last_login.date()
+            thirty_days_ago = today - timedelta(days=30)
+            if last_login_date <= thirty_days_ago:
+                user_login.is_active = False
+                user_login.save()
