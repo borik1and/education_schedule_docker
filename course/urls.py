3266196fe47908cc57700cd800fb9be9fed7e3b6
Boris <borik1and@gmail.com>
2024-03-16 18:15:33 +0200

    changing code

--- course/urls.py
+++ course/urls.py
@@ -1,7 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from django.urls import path
 from course.views import CourseViewSet
-from lesson.views import CourseSubscriptionView
 
 app_name = 'course'
 
